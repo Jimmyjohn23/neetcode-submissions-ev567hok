bf9583466a120c1d046439261d423feecb78097a
Jordan Mckoy <163925162+Jimmyjohn23@users.noreply.github.com>
2026-06-13 13:04:20 -0400

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> countBits(int n) {
+       vector<int> result(n + 1);
+        result[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            result[i] = __builtin_popcount(i);
+        }
+        
+        return result;
+    }
+    // int countOne(int n) {
+    //     int count = 0;
+    //     while (n) {
+    //         n &= (n - 1);
+    //         count++;
+    //     }
+    //     return count;
+    // }
+};
