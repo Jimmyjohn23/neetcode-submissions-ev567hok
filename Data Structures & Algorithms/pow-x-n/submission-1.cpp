028e18c0986e1f90c86dccc06f0a6d417d8f5483
Jordan Mckoy <163925162+Jimmyjohn23@users.noreply.github.com>
2026-06-15 14:34:15 -0400

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    double myPow(double x, int n) {
+        long long N = n;
+        double result = 1.0;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return result;
+    }
+};
